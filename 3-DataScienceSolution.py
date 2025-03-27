eb76e806aa880fc0b8a0fccf7b060c72d93be99b
dani88cala <daniele.calabrese@hotmail.it>
2025-03-27 16:50:08 +0100

    script 3 and update README

---
+++ 3-DataScienceSolution.py
@@ -0,0 +1,64 @@
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+#Aquire and show  data
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+
+combine = [train_df, test_df]
+#print(train_df.columns.values)
+print('Train dataset info:')
+train_df.info()
+print('-'*40)
+print('Test dataset info:')
+test_df.info()
+print('-'*40)
+print('Train dataset descibed:', '\n', train_df.describe())
+print('-'*40)
+print('Train dataset descibed:', '\n', train_df.describe(include='O'))
+print('-'*40)
+
+# Analyze by pivoting features
+print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print('-'*40)
+print(train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print('-'*40)
+print(train_df[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print('-'*40)
+print(train_df[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print('-'*40)
+
+# Analyze by visualizing data
+gAge = sns.FacetGrid(train_df, col='Survived')
+gAge.map(plt.hist, 'Age', bins=20)
+plt.show()
+
+gClass = sns.FacetGrid(train_df, col='Survived', row='Pclass', aspect=1.6)
+gClass.map(plt.hist, 'Age', alpha=.5, bins=20)
+gClass.add_legend()
+plt.show()
+
+gEmbarked = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+gEmbarked.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+gEmbarked.add_legend()
+plt.show()
+
+# Wrangle data
+
+
+
+
--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 Let's write some simple scripts just to train our skill in ML.
 
 # Reference
-Good train for this dataset:
+Script in this folder use the following dataset:
 https://www.kaggle.com/competitions/titanic
 
 # Explanations
 
+- 3-DataScienceSolution.py (Titanic Data Science Solutions): https://www.kaggle.com/code/startupsci/titanic-data-science-solutions/notebook
+
+    ### Note:
+    - Some instruction are not correct.
+    
+    ### Aquiring and show data
+    As usual. Here we also show info of our dataset with df.info() and df.describe(...)
+
+    ### Analyze by pivoting features
+    We can quickly analyze our feature correlations by pivoting features against each other.
+    We can see that the suvival field is correlated with Pclass, Sex, SibSp (sibling or spouse) and Parch (parents or child).
+
+    ### Analyze by visualizing data
+    Let us start by understanding correlations between numerical features and our solution goal (Survived).
+
+
+
+
 - 2-2-TF-DF.py (Titanic competition w/ TensorFlow Decision Forests): https://www.kaggle.com/code/gusthema/titanic-competition-w-tensorflow-decision-forests/comments
 
-## Note:
-On March 2025, Tensorflow package is not compatible with python 3.13 (https://github.com/tensorflow/tensorflow/issues/78774#issuecomment-2498148533). So this project is written with Google Colab
+    ### Note:
+    On March 2025, Tensorflow package is not compatible with python 3.13 (https://github.com/tensorflow/tensorflow/issues/78774#issuecomment-2498148533). So this project is written with Google Colab.
 
 - 1-tutorial.py:
     https://www.kaggle.com/code/alexisbcook/titanic-tutorial/notebook
